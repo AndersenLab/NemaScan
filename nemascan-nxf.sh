27f6f1a40b223da51ac20a7bbb8c06e339376105
Sam Wachspress <samuel.wachspress@northwestern.edu>
2021-10-05 12:53:35 -0500

    last updates to v1 hopefully

--- nemascan-nxf.sh
+++ nemascan-nxf.sh
@@ -4,6 +4,23 @@
 #
 ###################################################################################################################
 
+DEFAULT_DATA_DIR="gs://nf-pipelines/NemaScan/input_data"
+DEFAULT_VCF_VERSION="20210121"
+
+# Environment variables with default values:
+
+if [[ -z "${VCF_VERSION}" ]]; then
+  VCF_VERSION=${DEFAULT_VCF_VERSION}
+  echo "VCF_VERSION environment variable is not set - defaulting to ${VCF_VERSION}"
+fi
+
+if [[ -z "${DATA_DIR}" ]]; then
+  DATA_DIR=${DEFAULT_DATA_DIR}
+  echo "DATA_DIR environment variable is not set - defaulting to ${DATA_DIR}"
+fi
+
+
+# Environment variables that MUST be set
 
 if [[ -z "${TRAIT_FILE}" ]]; then
   echo "TRAIT_FILE environment variable must be set to the Google Storage path of the data"
@@ -20,10 +37,6 @@ if [[ -z "${WORK_DIR}" ]]; then
   exit 1
 fi
 
-if [[ -z "${VCF_VERSION}" ]]; then
-  VCF_VERSION="20210121"
-  echo "VCF_VERSION environment variable is not set - defaulting to ${VCF_VERSION}"
-fi
 
 nextflow run main.nf \
   -profile gcp \
@@ -31,4 +44,4 @@ nextflow run main.nf \
   --vcf "${VCF_VERSION}" \
   --work_dir "${WORK_DIR}" \
   --out "${OUTPUT_DIR}" \
-  --data_dir "gs://nf-pipelines/NemaScan/new_input_data/input_data/"
+  --data_dir "${DATA_DIR}"
